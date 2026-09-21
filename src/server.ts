7207e075ffd270323af8b4f123047cacebfe96d3
John <polatsultonov97@gmail.com>
2026-09-22 01:42:01 +0900

    fix: removing some lines of codes inside server.ts

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-// console.log("EXECUTED");
-
-// import moment from 'moment';
-
-// const currentTime = moment().format("YYYY MM DD HH:MM:SS");
-// console.log(currentTime);
-
-// const person: string = "John";
-// const count: number = 100;
