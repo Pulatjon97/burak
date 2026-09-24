ecc3512e0d6d43b5b0060069349f048cb9481c05
John <polatsultonov97@gmail.com>
2026-09-24 22:04:42 +0900

    feat: Task - M

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+//  Task - M
+// npm run train
+
+function getSquareNumbers(arr) {
+  return arr.map(number => ({
+    number: number,
+    square: number * number
+  }));
+}
+
+// Test
+console.log(getSquareNumbers([4, 8, 10]));
