3a0fff892bd32eafd59ba3eb6862c33206b3e907
John <polatsultonov97@gmail.com>
2026-09-26 03:17:28 +0900

    fix: modify routing system and controllers

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,10 @@ const router = express.Router();
 import membercontroller from './controllers/member.controller';
 import memberController from "./controllers/member.controller";
 
-router.get('/', memberController.goHome);
+// router.get('/', memberController.goHome);
 
-router.get('/login', memberController.getLogin);
+// router.get('/login', memberController.getLogin);
 
-router.get('/signup', memberController.getSignup);
+// router.get('/signup', memberController.getSignup);
 
 export default router;
